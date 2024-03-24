1688773ac0d535c68c9361eb92205ad332e7a3dd
anilyavas <anil.yavas99@gmail.com>
2024-03-24 17:03:42 +0300

    install and setup stream video

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,6 +10,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
 import { useColorScheme } from '@/src/components/useColorScheme';
+import { PermissionsAndroid, Platform } from 'react-native';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -40,6 +41,18 @@ export default function RootLayout() {
       SplashScreen.hideAsync();
     }
   }, [loaded]);
+
+  useEffect(() => {
+    const run = async () => {
+      if (Platform.OS === 'android') {
+        await PermissionsAndroid.requestMultiple([
+          'android.permission.POST_NOTIFICATIONS',
+          'android.permission.BLUETOOTH_CONNECT',
+        ]);
+      }
+    };
+    run();
+  }, []);
 
   if (!loaded) {
     return null;
